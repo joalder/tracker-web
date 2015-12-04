254b64ff1b5746beb65ceb59b1a2e0e0f5d0bd02
joalder <jonas.alder@gmail.com>
2015-12-04 23:40:12 +0100

    - improved domain pojos

--- src/main/java/ch/vilalde/tracker/web/domain/Project.java
+++ src/main/java/ch/vilalde/tracker/web/domain/Project.java
@@ -1,16 +1,14 @@
 package ch.vilalde.tracker.web.domain;
 
+import java.util.ArrayList;
+
 /**
  * Project domain POJO
  */
 public class Project {
     private String name;
     private String color;
-
-    public Project(String name, String color) {
-        this.name = name;
-        this.color = color;
-    }
+    private ArrayList<Task> tasks;
 
     public String getName() {
         return name;
@@ -26,5 +24,23 @@ public class Project {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task){
+        if(task != null) {
+            tasks.add(task);
+        }
+    }
+
+    public boolean isValid() {
+        return name != null && color != null;
     }
 }
--- src/main/java/ch/vilalde/tracker/web/domain/Task.java
+++ src/main/java/ch/vilalde/tracker/web/domain/Task.java
@@ -1,7 +1,60 @@
 package ch.vilalde.tracker.web.domain;
 
 /**
- * Created by Jonas on 12/1/2015.
+ * Task domain POJO
  */
 public class Task {
+
+    private String title;
+    private int effortEstimated;
+    private int effortSpent;
+    private String priority;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getEffortEstimated() {
+        return effortEstimated;
+    }
+
+    public void setEffortEstimated(int effortEstimated) {
+        this.effortEstimated = effortEstimated;
+    }
+
+    public int getEffortSpent() {
+        return effortSpent;
+    }
+
+    public void setEffortSpent(int effortSpent) {
+        this.effortSpent = effortSpent;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isValid() {
+        return title != null && !title.equals("") &&
+                effortEstimated != 0 &&
+                description != null && !description.equals("") &&
+                priority != null && !priority.equals("");
+    }
 }
